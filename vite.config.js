2e8f11779221f660a13e3f49e8ff0eadc98426b3
twiggy22 <twiggy22@seznam.cz>
2025-11-21 12:56:15 +0100

    Zmena base URL pro vlastni domenu (skola.iteb.cz)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/app_skola/', // DŮLEŽITÉ: Změňte na název vašeho repozitáře na GitHubu (např. '/moje-skola/')
+  base: '/', // Pro vlastní doménu (např. skola.iteb.cz) musí být base '/'
 })
